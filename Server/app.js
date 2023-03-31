d0029d9d6a8c9778a8da21aefa61b02a57890945
Celine Amarachi <94835448+CelineAma@users.noreply.github.com>
2023-03-31 11:08:43 +0100

    Update app.js
    updated comment

--- Server/app.js
+++ Server/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+//serve static files (css, javascript) from the "Client" folder
 app.use(express.static(path.join(__dirname, "Client"))); //to make app aware of the static files
 
+//serve the chatRoom.html file
 app.get("/", (req, res) => {
     res.status(200).sendFile(path.join(__dirname, "Client", "chatRoom.html")) //render the static files
 });
